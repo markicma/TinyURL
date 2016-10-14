e7c79162ba141eaad7776d4c52aca4efce4e3fd1
Matthew Markic <matt.markic@hotmail.com>
2016-10-14 20:34:43 +0000

    Added comments to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 'use strict';
+// Below are the various packages used within this server
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
@@ -14,9 +15,14 @@ app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
 
+// Below are the two databases that are used to keep track of users as well as
+// shortened links
 let users = {};
 let urlDatabase = {};
 
+// If a user is logged in while the server handles a get request for the root
+// endpoint the user is redirected to the /urls endpoint. If the user isn't
+// logged in the user is redirected to the /login endpoint.
 app.get("/", (req, res) => {
   if (req.session.user_id) {
     res.redirect("/urls");
@@ -25,6 +31,10 @@ app.get("/", (req, res) => {
   }
 })
 
+// If a user is logged in while the server handles a get request for the /urls
+// endpoint, the page urls_index.ejs is rendered while being able to access the
+// templateVars object. If the user isn't logged in a 401 status code is sent
+// back and the user is shown an error page.
 app.get("/urls", (req, res) => {
   if (req.session.user_id) {
     let templateVars = {
@@ -40,6 +50,10 @@ app.get("/urls", (req, res) => {
   }
 })
 
+// If a user isn't logged in while the server handles a get request for the
+// /urls/new endpoint, a 401 status code is sent back and the user is shown an
+// error page. If the user is logged in, the urls_new.ejs file is rendered with
+// access to templateVars object.
 app.get("/urls/new", (req, res) => {
   if (!req.session.user_id) {
     res.statusCode = 401;
@@ -54,6 +68,15 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+// When the server handles get requests to display a specific tinyURL, if
+// a user tries to display a tinyURL that isn't created a 404 status code is
+// sent and an error message is displayed. If a user tries to display a tinyURL
+// and isn't logged in, a 401 status code is sent and an error message is
+// displayed. If a user is logged in and tries to display a tinyURL that they
+// didn't make, then a 403 status code is sent back and an error message is
+// displayed. Lastly, if a user is logged in and tries to display a tinyURL that
+// they created, the urls_show.ejs document is rendered and has access to the
+// templateVars object.
 app.get("/urls/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     res.statusCode = 404;
@@ -76,6 +99,10 @@ app.get("/urls/:id", (req, res) => {
   }
 })
 
+// When the server handles get requests to redirect from the endpoint /u/:id to
+// the associated long url, if the tinyURL has been created then the user is
+// redirected to the website with the long url. If the tinyURL doesn't exist
+// then a 404 status code is sent and an error message appears on the page
 app.get("/u/:id", (req, res) => {
   if (urlDatabase[req.params.id]) {
     let longURL = urlDatabase[req.params.id];
@@ -86,6 +113,14 @@ app.get("/u/:id", (req, res) => {
   }
 })
 
+// When the server handles post requests to add new tinyURLs, if the user is
+// logged in, a random 6 digit string is generated and the variable user_id
+// takes on the cookie value. The random string is added to the urlDatabase as
+// a key and the long url as the value. Using the user_id, the random string is
+// added as a key to the corresponding user in a key called links which is an
+// object of tinyURLs created by that user with corresponding long URL values.
+// The user is then redirected to the /urls/${rand} endpoint. If the user isn't
+// logged in a 401 status code is sent and an error message is displayed on the page.
 app.post("/urls", (req, res) => {
   if (req.session.user_id) {
     const rand = generateRandomString();
@@ -99,6 +134,17 @@ app.post("/urls", (req, res) => {
   }
 })
 
+// When the server handles posts requests to the /urls/:id endpoint, used to
+// update tinyURLs, if the tinyURL doesn't exist a 404 status code is sent and
+// an error message shows up on the page. If a user isn't logged in, a 401 status
+// code is sent and an error message shows up on the page. If a user is logged
+// in but the tinyURL doesn't belong to that user a 403 status code is sent and
+// an error message shows up on the page. If the user is logged in and the tinyURL
+// belongs to that user, the cookie is stored in the user_id variable and the
+// value in the url database corresponding to the id parameter is updated to the
+// new url that was submitted in the form. The value in the corresponding 
+// user's links object is also updated to the new url that was submitted. The
+// user is then redirected to the /urls endpoint.
 app.post("/urls/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     res.statusCode = 404;
@@ -117,6 +163,11 @@ app.post("/urls/:id", (req, res) => {
   }
 })
 
+// When the server handles post requests to the /urls/:id/delete endpoint,
+// the cookie is assigned to a variable user_id. The appropriate user is then
+// identified from the users database and the corresponding key in user's links
+// object is deleted. The corresponding key in the urlDatabase is also deleted.
+// The user is then redirected to the /urls endpoint.
 app.post("/urls/:id/delete", (req, res) => {
   const user_id = req.session.user_id;
   delete users[user_id].links[req.params.id];
@@ -124,6 +175,9 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 })
 
+// When the server handles get requests for the /login endpoint, if a user is
+// logged in, the user is redirected to the /login endpoint. If the user isn't
+// logged in, the urls_login.ejs file is rendered.
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/');
@@ -132,6 +186,9 @@ app.get("/login", (req, res) => {
   }
 })
 
+// When the server handles get requests for the /register endpoint, if a user is
+// logged in, the user is redirected to the root endpoint. If the user isn't
+// logged in the registe_form.ejs file is rendered.
 app.get("/register", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/');
@@ -141,7 +198,19 @@ app.get("/register", (req, res) => {
   }
 })
 
-app.post("/register", (req, res, next) => {
+// When the server handles post requests to the /register endpoint, the key for
+// a user is found in the user database using the email provided in the form. If
+// no information is filled in either the email or password fields a 400 status
+// code is sent and an error message is displayed on the page. If a the email
+// put in the email field already exists in the user database a 400 status code
+// is sent and an error message is displayed on the page. If the form is filled
+// out correctly a random string is generated and a new user is created in the
+// users database using the random string as the key to an object with the
+// id, email (containing the entered email), password (containing the hashed
+// version of the entered password), and links (an object of links the user
+// creates). An encrypted cookie is then created using the random string and
+// the user is redirected to the root endpoint.
+app.post("/register", (req, res) => {
   const key = _.findKey(users, { 'email': req.body.email });
   if (!req.body.email || !req.body.password) {
     res.statusCode = 400;
@@ -162,7 +231,14 @@ app.post("/register", (req, res, next) => {
   }
 })
 
-app.post("/login", (req, res, next) => {
+// When the server handles post requests to the /login endpoint, using the variable
+// key the user is found in the database by their email. If the user exists and
+// the hashed entered password matches the hashed stored password, an encrypted
+// cookie is then set as the key that was used to find the user in the user
+// database. The user is then redirected to the root endpoint. If the email and
+// password don't match the values found in the user database a 401 status code
+// is sent and an error message is shown on the page.
+app.post("/login", (req, res) => {
   const key = _.findKey(users, { 'email': req.body.email });
   if (users[key] && bcrypt.compareSync(req.body.password, users[key].password)) {
     req.session.user_id = key;
@@ -173,15 +249,20 @@ app.post("/login", (req, res, next) => {
   }
 })
 
+// When the server handles post requests to the /logout endpoint, the cookies
+// are cleared from the browser and the user is redirected to the root endpoint
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/");
 })
 
+// The server is listening for incoming requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
 
+// The function below generates a 6 character randomizes string using capital
+// and lowercase letters as well as numbers.
 function generateRandomString() {
   let result = '';
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
